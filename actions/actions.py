0b9e14a1d35d0cd825e68c6315d0704cf92d88b8
Amudhesh-KT <damudheshkt@gmail.com>
2023-12-13 14:02:47 +0530

    nlu changes

--- actions/actions.py
+++ actions/actions.py
@@ -4,6 +4,8 @@ from rasa_sdk.executor import CollectingDispatcher
 from typing import Any, Text, Dict, List
 from rasa_sdk.events import UserUtteranceReverted
 import json
+from rasa_sdk.events import SlotSet
+
 
 
 class ActionGreet(Action):
@@ -33,227 +35,10 @@ class ActionGoodbye(Action):
  
         return []
     
-class ActionRetriveTP(Action):
-
-    def name(self) -> Text:
-        return "action_retrive_tp"
-
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        
-        project_name = tracker.get_slot('project_name')
-        customer_name = tracker.get_slot('customer_name')
-        doc_type = tracker.get_slot('doc_type')
-        res = {
-            'Title': "Retrive TP Document",
-            'Project name': project_name,
-            'Customer name': customer_name,
-            'Document type': doc_type
-        }
-        msg = json.dumps(res)
-        print(msg)
-        dispatcher.utter_message(text=msg)
-
-        return []
-    
-class ActionRetriveCP(Action):
-
-    def name(self) -> Text:
-        return "action_retrive_cp"
-
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-
-        project_name = tracker.get_slot('project_name')
-        customer_name = tracker.get_slot('customer_name')
-        doc_type = tracker.get_slot('doc_type')
-        res = {
-            'Title': "Retrive CP Document",
-            'Project name': project_name,
-            'Customer name': customer_name,
-            'Document type': doc_type
-        }
-        msg = json.dumps(res)
-        print(msg)
-        dispatcher.utter_message(text=msg)
-
-        return []
-    
-class ActionRetriveFRS(Action):
-
-    def name(self) -> Text:
-        return "action_retrive_frs"
-
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-
-        project_name = tracker.get_slot('project_name')
-        customer_name = tracker.get_slot('customer_name')
-        doc_type = tracker.get_slot('doc_type')
-        res = {
-            'Title': "Retrive FRS Document",
-            'Project name': project_name,
-            'Customer name': customer_name,
-            'Document type': doc_type
-        }
-        msg = json.dumps(res)
-        print(msg)
-        dispatcher.utter_message(text=msg)
-
-        return []
-
-class ActionRetriveRFP(Action):
-
-    def name(self) -> Text:
-        return "action_retrive_rfp"
-
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-
-        project_name = tracker.get_slot("project_name")
-        print(project_name)
-        customer_name = tracker.get_slot('customer_name')
-        doc_type = tracker.get_slot('doc_type')
-        res = {
-            'Title': "Retrive RFP Document",
-            'Project name': project_name,
-            'Customer name': customer_name,
-            'Document type': doc_type
-        }
-        msg = json.dumps(res)
-        print(msg)
-        dispatcher.utter_message(text=msg)
-
-        return []
-
-class ActionRetriveSDD(Action):
-
-    def name(self) -> Text:
-        return "action_retrive_sdd"
-
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-
-        project_name = tracker.get_slot('project_name')
-        customer_name = tracker.get_slot('customer_name')
-        doc_type = tracker.get_slot('doc_type')
-        res = {
-            'Title': "Retrive SDD Document",
-            'Project name': project_name,
-            'Customer name': customer_name,
-            'Document type': doc_type
-        }
-        msg = json.dumps(res)
-        print(msg)
-        dispatcher.utter_message(text=msg)
-
-        return []
-    
-class ActionRetriveUAT(Action):
-
-    def name(self) -> Text:
-        return "action_retrive_uat"
-
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-
-        project_name = tracker.get_slot('project_name')
-        customer_name = tracker.get_slot('customer_name')
-        doc_type = tracker.get_slot('doc_type')
-        res = {
-            'Title': "Retrive UAT Document",
-            'Project name': project_name,
-            'Customer name': customer_name,
-            'Document type': doc_type
-        }
-        msg = json.dumps(res)
-        print(msg)
-        dispatcher.utter_message(text=msg)
-
-        return []
-    
-class ActionRetriveCutover(Action):
-
-    def name(self) -> Text:
-        return "action_retrive_cutover"
-
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-
-        project_name = tracker.get_slot('project_name')
-        customer_name = tracker.get_slot('customer_name')
-        doc_type = tracker.get_slot('doc_type')
-        res = {
-            'Title': "Retrive cutover Document",
-            'Project name': project_name,
-            'Customer name': customer_name,
-            'Document type': doc_type
-        }
-        msg = json.dumps(res)
-        print(msg)
-        dispatcher.utter_message(text=msg)
-
-        return []
-    
-class ActionRetriveSignoff(Action):
-
-    def name(self) -> Text:
-        return "action_retrive_signoff"
-
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-
-        project_name = tracker.get_slot('project_name')
-        customer_name = tracker.get_slot('customer_name')
-        doc_type = tracker.get_slot('doc_type')
-        res = {
-            'Title': "Retrive signoff Document",
-            'Project name': project_name,
-            'Customer name': customer_name,
-            'Document type': doc_type
-        }
-        msg = json.dumps(res)
-        print(msg)
-        dispatcher.utter_message(text=msg)
-
-        return []
-    
-class ActionRetriveAlldocs(Action):
-
-    def name(self) -> Text:
-        return "action_retrive_alldocs"
-
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-
-        project_name = tracker.get_slot('project_name')
-        customer_name = tracker.get_slot('customer_name')
-
-        res = {
-            'Title': "Retrive All Documents",
-            'Project name': project_name,
-            'Customer name': customer_name,
-
-        }
-        msg = json.dumps(res)
-        print(msg)
-        dispatcher.utter_message(text=msg)
-
-        return []
-    
 class ActionProjectList(Action):
 
     def name(self) -> Text:
-        return "action_project_list"
+        return "projectlist_action"
 
     def run(self, dispatcher: CollectingDispatcher,
             tracker: Tracker,
@@ -268,5 +53,27 @@ class ActionProjectList(Action):
         print(msg)
         dispatcher.utter_message(text=msg)
 
-        return []
+        return [SlotSet("customer_name", None)]
     
+class ActionAlldocList(Action):
+
+    def name(self) -> Text:
+        return "alldoclist_action"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        customer_name = tracker.get_slot('customer_name')
+        project_name = tracker.get_slot('project_name')
+        
+        res = {
+            'Title': "Project list",
+            'Customer name': customer_name,
+            'Project name': project_name
+        }
+        msg = json.dumps(res)
+        print(msg)
+        dispatcher.utter_message(text=msg)
+
+        return [SlotSet("customer_name", None),SlotSet("project_name", None)]
